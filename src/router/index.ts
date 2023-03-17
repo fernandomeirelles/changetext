d59cb67ca4d5fd3dd634d49cfe05f1781e58f023
Fernando Meirelles <fernandomspereira@gmail.com>
2023-03-17 20:32:51 -0300

    Mudar nome do projeto

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+//import HomeView from '../views/HomeView.vue'
+import Transform from '../views/TransformView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
+    /*{
       path: '/',
       name: 'home',
       component: HomeView
@@ -16,11 +17,12 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    },
+    },*/
     {
-      path: '/transform',
+      path: '/',
       name: 'transform',
-      component: () => import('../views/Transform.vue')
+      //component: () => import('../views/TransformView.vue')
+      component: Transform
     },
   ]
 })
