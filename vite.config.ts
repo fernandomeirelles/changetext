830a8a6acb30452cc556ddb98df5bd8830cd7ab7
Fernando Meirelles <fernandomspereira@gmail.com>
2023-03-21 12:43:59 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/changetext/',
   plugins: [vue()],
+  build: {
+    target: 'esnext'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
