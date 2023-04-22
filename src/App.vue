9e21622bffb29d5702439a1aa54312c40d102f70
Fernando Meirelles <fernandomspereira@gmail.com>
2023-04-22 01:02:33 -0300

    Ajustar meta description

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ export default {
         title: title,
         description: description,
         type: 'website',
-        image: 'https://fernandomeirelles.github.io/changetext/meta-image.jpg',
+        image: '/meta-image.jpg',
         "image:type": 'image/jpeg',
         "image:width": '1200',
         "image:height": '630',
@@ -26,7 +26,7 @@ export default {
       twitter: {
         title: title,
         description: description,
-        image: 'https://fernandomeirelles.github.io/changetext/meta-image.jpg',
+        image: '/meta-image.jpg',
       },
         htmlAttrs: { lang: 'pt-br', amp: true }
       },
